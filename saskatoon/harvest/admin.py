625cebbb9745e7e286a17fcf64661d3f8cdb14ad
Tiago Bortoletto Vaz <tiago@debian.org>
2021-03-28 18:02:42 -0400

    Add views to admin

--- saskatoon/harvest/admin.py
+++ saskatoon/harvest/admin.py
@@ -1,20 +1,75 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from leaflet.admin import LeafletGeoAdmin
 from django.contrib import admin
+
 from member.models import *
 from harvest.models import *
+from harvest.forms import *
 
-admin.site.register(Property)
-admin.site.register(Harvest)
-admin.site.register(RequestForParticipation)
+class PropertyInline(admin.TabularInline):
+    model = Property
+    extra = 0
+
+class PersonInline(admin.TabularInline):
+    model = RequestForParticipation
+    verbose_name = "Cueilleurs pour cette récolte"
+    verbose_name_plural = "Cueilleurs pour cette récolte"
+    form = RFPForm
+    exclude = ['creation_date', 'confirmation_date']
+    extra = 3
+
+
+class OrganizationAdmin(admin.ModelAdmin):
+    inlines = [
+        PropertyInline,
+    ]
+    search_fields = ['name', 'description']
+
+
+class HarvestYieldInline(admin.TabularInline):
+    model = HarvestYield
+    form = HarvestYieldForm
+
+
+class HarvestAdmin(admin.ModelAdmin):
+    # form = HarvestForm
+    model = Harvest
+    inlines = (PersonInline, HarvestYieldInline)
+
+
+class RequestForParticipationAdmin(admin.ModelAdmin):
+    form = RFPForm
+
+
+class EquipmentAdmin(admin.ModelAdmin):
+    form = EquipmentForm
+
+
+class PropertyImageInline(admin.TabularInline):
+    model = PropertyImage
+    extra = 3
+
+
+class PropertyAdmin(LeafletGeoAdmin):
+    model = Property
+    inlines = [PropertyImageInline]
+    form = PropertyForm
+
+admin.site.register(Property, PropertyAdmin)
+admin.site.register(Harvest, HarvestAdmin)
+admin.site.register(RequestForParticipation, RequestForParticipationAdmin)
 admin.site.register(TreeType)
-admin.site.register(Equipment)
+admin.site.register(Equipment, EquipmentAdmin)
 admin.site.register(EquipmentType)
 admin.site.register(HarvestYield)
 admin.site.register(Comment)
+admin.site.register(PropertyImage)
 
+admin.site.register(AuthUser)
 admin.site.register(Actor)
 admin.site.register(Language)
 admin.site.register(Person)
