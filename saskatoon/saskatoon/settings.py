3145f53a56872cacd96ba94b6a7ba397cace1ef2
Tassia Camoes Araujo <tassia@acaia.ca>
2021-03-28 20:41:56 +0000

    Add i18n support and basic translations

--- saskatoon/saskatoon/settings.py
+++ saskatoon/saskatoon/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,7 +126,21 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
+
+LANGUAGES = [
+    ('fr',u'Français'),
+    ('en',u'English'),
+]
+
+LOCALE_PATHS = [
+    'harvest/locale/',
+    'member/locale/',
+    'sitebase/locale/',
+    'saskatoon/locale/'
+]
+
+CSRF_COOKIE_SECURE = True
 
 TIME_ZONE = 'UTC'
 
