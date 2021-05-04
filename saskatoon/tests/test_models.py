4f793ab59c827d5e8b1e4993aec47d182f9d240d
tristanlatr <tris.la.tr@gmail.com>
2021-05-04 17:39:48 -0400

    Delete models tests for now

--- saskatoon/tests/test_models.py
+++ saskatoon/tests/test_models.py
@@ -5,9 +5,3 @@ from django.test import TestCase
 # from harvest.models import (Property, Harvest, RequestForParticipation, TreeType, 
 #                             Equipment, EquipmentType, HarvestYield, Comment, PropertyImage)
 
-class PropertyTestCase(TestCase):
-    def setUp(self):
-        pass
-
-    def test_property(self):
-        pass
