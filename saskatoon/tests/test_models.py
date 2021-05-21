6fcdd681bf79f3c175d577708b2816d55e5fe3a5
tristanlatr <19967168+tristanlatr@users.noreply.github.com>
2021-05-20 20:29:16 -0400

    delete test_models.py

--- saskatoon/tests/test_models.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-# This import causes RuntimeError: Model class harvest.models.TreeType doesn't declare an explicit app_label and isn't in an application in INSTALLED_APPS
-# See https://medium.com/@michal.bock/fix-weird-exceptions-when-running-django-tests-f58def71b59a
-# from harvest.models import (Property, Harvest, RequestForParticipation, TreeType, 
-#                             Equipment, EquipmentType, HarvestYield, Comment, PropertyImage)
-
