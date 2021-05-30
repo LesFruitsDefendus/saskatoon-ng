d207b3bc56c2f649123a0b47f3072402d4936fce
Tiago Bortoletto Vaz <tiago@debian.org>
2021-05-30 10:14:17 -0400

    Add wsgi file for uwsgi deployment.

--- saskatoon/member/migrations/0001_initial.py
+++ saskatoon/member/migrations/0001_initial.py
@@ -8,9 +8,9 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-        ('auth', '0012_alter_user_first_name_max_length'),
-    ]
+#    dependencies = [
+#        ('auth', '0012_alter_user_first_name_max_length'),
+#    ]
 
     operations = [
         migrations.CreateModel(
--- saskatoon/saskatoon/static/js/data-table/data-table-act.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
- "use strict";
-	$(document).ready(function() {
-    $('#data-table-basic').DataTable( {
-        "order": [[ 0, "desc" ]]
-    } );
-} );
-})(jQuery); 
