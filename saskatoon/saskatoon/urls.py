4e65d9712e8e3075a7971502ce2bd66d3cd3f8d8
2ynn <qynn@divo.id>
2021-06-04 10:17:38 -0400

    added debug toolbar

--- saskatoon/saskatoon/urls.py
+++ saskatoon/saskatoon/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+import debug_toolbar
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('harvest.urls')),
     path('', include('sitebase.urls')),
-    path('',include('django.contrib.auth.urls'))
-
+    path('',include('django.contrib.auth.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
