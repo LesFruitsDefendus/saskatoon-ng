a53deb317fadf3d1c1d6e6bf6052befb5b9ba1ae
2ynn <qynn@divo.id>
2021-06-06 10:49:21 -0400

    forgot to add the confirm delete script

---
+++ saskatoon/sitebase/static/js/confirm/delete.js
@@ -0,0 +1,8 @@
+
+function confirmDelete() {
+    if (confirm('Are you sure you want to delete this entry?')) {
+        // console.log("Entry Deleted");
+    } else {
+        return false;
+    }
+}
