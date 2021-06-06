b62eeb14bac67a9456cff02493cdd1ee9a41dad9
2ynn <qynn@divo.id>
2021-06-06 16:46:35 -0400

    fixed confirm dialog

--- saskatoon/sitebase/static/js/confirm/delete.js
+++
@@ -1,8 +0,0 @@
-
-function confirmDelete() {
-    if (confirm('Are you sure you want to delete this entry?')) {
-        // console.log("Entry Deleted");
-    } else {
-        return false;
-    }
-}
