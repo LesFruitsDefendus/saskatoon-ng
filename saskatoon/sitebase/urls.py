2819efb68f8dedde5dc65a9aa4fe4486f9ffde66
2ynn <qynn@divo.id>
2021-07-24 16:07:56 -0400

    added reset password urls

--- saskatoon/sitebase/urls.py
+++ saskatoon/sitebase/urls.py
@@ -10,24 +10,20 @@ urlpatterns = [
     url(r'^calendar$', views.Calendar.as_view(), name='calendar'),
     url(r'^jsoncal', views.JsonCalendar.as_view(), name='calendarJSON'),
 
-    # url(
-    #     r'^password_reset/$',
-    #     auth_views.PasswordResetView,
-    #     name='password_reset'
-    # ),
-    # url(
-    #     r'^password_reset/done/$',
-    #     auth_views.PasswordResetDoneView,
-    #     name='password_reset_done'
-    # ),
-    # url(
-    #     r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-    #     auth_views.PasswordResetConfirmView,
-    #     name='password_reset_confirm'
-    # ),
-    # url(
-    #     r'^reset/done/$',
-    #     auth_views.PasswordResetCompleteView,
-    #     name='password_reset_complete'
-    # ),
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(),
+         name ='reset_password'),
+
+    path('reset_password_sent/',
+         auth_views.PasswordResetDoneView.as_view(),
+         name ='password_reset_done'),
+
+    path('reset/<uidb64>/<token>',
+         auth_views.PasswordResetConfirmView.as_view(),
+         name ='password_reset_confirm'),
+
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(),
+         name ='password_reset_complete'),
+
 ]
