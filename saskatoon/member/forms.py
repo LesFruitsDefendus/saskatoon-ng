99b19f87fd5adaa40f83fb4a3334f7125f654dc6
Qynn Swaan <41802320+2ynn@users.noreply.github.com>
2021-08-02 19:17:39 -0400

    added contact person registration along with Organization form

--- saskatoon/member/forms.py
+++ saskatoon/member/forms.py
@@ -7,6 +7,10 @@ from harvest.models import Property
 from member.models import AuthUser, Person, Organization, AUTH_GROUPS, STAFF_GROUPS
 from django.contrib.auth.models import Group
 
+
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column
+
 def set_person_roles(person, roles):
     ''' updates auth_user groups
         :param person: Person instance
@@ -20,6 +24,14 @@ def set_person_roles(person, roles):
 
     auth_user.is_staff = any([r in STAFF_GROUPS for r in roles])
     auth_user.save()
+
+def validate_email(email):
+    ''' check if a user with same email address is already registered'''
+    if AuthUser.objects.filter(email=email).exists():
+        raise forms.ValidationError(
+            _("ERROR: email address < {} > is already registered!").format(email)
+        )
+    return email
 
 class PersonCreateForm(forms.ModelForm):
 
@@ -47,11 +59,7 @@ class PersonCreateForm(forms.ModelForm):
     field_order = ['roles', 'first_name', 'family_name', 'email', 'language']
 
     def clean_email(self):
-        ''' check if email address already exists'''
-        email = self.cleaned_data['email']
-        if AuthUser.objects.filter(email=email).exists():
-            raise forms.ValidationError("This email address is already registered")
-        return email
+        return validate_email(self.cleaned_data['email'])
 
     def save(self):
 
@@ -64,6 +72,7 @@ class PersonCreateForm(forms.ModelForm):
                 person=instance
         )
         auth_user.save()
+
         set_person_roles(instance, self.cleaned_data['roles'])
 
         # associate pending_property (if any)
@@ -109,13 +118,97 @@ class PersonUpdateForm(forms.ModelForm):
 
         return self.instance
 
+
 class OrganizationCreateForm(forms.ModelForm):
 
     class Meta:
         model = Organization
         exclude = ['redmine_contact_id', 'longitude', 'latitude']
         labels = {
-            'is_beneficiary': "Beneficiary organization",
+            'is_beneficiary': _("Beneficiary organization"),
+            'contact_person_role': _("Position/Role"),
+        }
+
+
+    contact_person = forms.ModelChoiceField(
+        queryset=Person.objects.all(),
+        label=_("Select Person"),
+        widget=autocomplete.ModelSelect2('contact-autocomplete'),
+        required=False,
+        # help_text="FDODFlasdflasfjaslf"
+    )
+
+    create_new_person = forms.BooleanField(
+        label=_("&nbsp;Register new contact person"),
+        required=False
+    )
+
+    contact_first_name = forms.CharField(
+        label=_("First Name"),
+        required=False
+    )
+
+    contact_last_name = forms.CharField(
+        label=_("Last Name"),
+        required=False
+    )
+
+    contact_email = forms.EmailField(
+        label=_("Email"),
+        required=False
+    )
+
+    contact_phone = forms.CharField(
+        label=_("Phone"),
+        required=False
+    )
+
+    def clean(self):
+        data = super().clean()
+        person = data['contact_person']
+        if not person:
+            if data['contact_email'] and data['contact_first_name']:
+                validate_email(data['contact_email'])
+            else:
+                raise forms.ValidationError(
+                    _("ERROR: You must either select a Contact \
+                    Person or create a new one and provide their personal information"))
+        return data
+
+
+    def save(self):
+        # # create Organization instance
+        instance = super(OrganizationCreateForm, self).save()
+
+        # # create Contact Person/AuthUser
+        person = Person.objects.create(
+            first_name=self.cleaned_data['contact_first_name'],
+            family_name=self.cleaned_data['contact_last_name'],
+            phone=self.cleaned_data['contact_phone'])
+        person.save()
+
+        auth_user = AuthUser.objects.create(
+            email=self.cleaned_data['contact_email'],
+            person=person)
+        auth_user.save()
+
+        set_person_roles(person, ['contact'])
+
+        # # associate Contact to Organization
+        instance.contact_person = person
+        instance.save()
+
+        return instance
+
+
+class OrganizationUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = Organization
+        exclude = ['redmine_contact_id', 'longitude', 'latitude']
+        labels = {
+            'is_beneficiary': _("Beneficiary organization"),
+            'contact_person_role': _("Contact Position/Role"),
         }
 
         widgets = {
@@ -123,41 +216,3 @@ class OrganizationCreateForm(forms.ModelForm):
                'contact-autocomplete'
             ),
         }
-
-    # field_order = ['civil_name', 'is_beneficiary', 'description']
-
-    # is_beneficiary = forms.BooleanField(
-    #     label=_("Beneficiary organization"),
-    #     required=False
-    # )
-
-
-    # def clean_email(self):
-    #     ''' check if email address already exists'''
-    #     email = self.cleaned_data['email']
-    #     if AuthUser.objects.filter(email=email).exists():
-    #         raise forms.ValidationError("This email address is already registered")
-    #     return email
-
-    # def save(self):
-
-        # # create Person instance
-        # instance = super(PersonCreateForm, self).save()
-
-        # # create associated auth.user
-        # auth_user = AuthUser.objects.create(
-        #         email=self.cleaned_data['email'],
-        #         person=instance
-        # )
-        # auth_user.save()
-        # set_person_roles(instance, self.cleaned_data['roles'])
-
-        # # associate pending_property (if any)
-        # pid = self.cleaned_data['pending_property_id']
-        # if pid:
-        #     try:
-        #         pending_property = Property.objects.get(id=pid)
-        #         pending_property.owner = instance
-        #         pending_property.save()
-        #     except Exception as e: print(e)
-        # return instance
