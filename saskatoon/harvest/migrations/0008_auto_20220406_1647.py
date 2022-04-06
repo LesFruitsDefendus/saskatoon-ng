01f4e6369dfae1d287d5f258c806e2fde6b70773
Farhan Masud Aneek <farhan.aneek@gmail.com>
2022-04-07 04:56:48 +0600

    pending_contact_name field split to pending_contact_first_name and pending_contact_family_name and update references of these fields on other files

--- saskatoon/harvest/migrations/0008_auto_20220406_1647.py
+++ saskatoon/harvest/migrations/0008_auto_20220406_1647.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('harvest', '0006_alter_equipment_shared'),
+        ('harvest', '0007_alter_property_pending_contact_phone'),
     ]
 
     operations = [
