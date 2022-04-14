b493e0de7f20118082fa1de698dc2e2574a79b42
tristanlatr <tris.la.tr@gmail.com>
2022-04-14 14:10:59 -0400

    Words

--- saskatoon/tests/newenvfile.py
+++ saskatoon/tests/newenvfile.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Command to create a new .env file based on a given sqlite_db_path.
+Command to create a new .env file based on a given sqlite_db_path (for testing only).
 """
 
 
@@ -24,7 +24,7 @@ SASKATOON_ADMIN_PASSWORD=
 if __name__ == "__main__":
     
     if len(sys.argv) != 2:
-        print("Usage: saskatoon/tests/newenvfile.py <sqlite_db_path> > saskatoon/new.env")
+        print("Usage: saskatoon/tests/newenvfile.py <sqlite_db_path> > saskatoon/.env")
         exit(1)
     
     db_path = pathlib.Path(sys.argv[1]).absolute()
