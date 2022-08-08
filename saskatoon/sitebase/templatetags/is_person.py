bab2ec31fbc25b15199868d3ad1c9c1f528e8184
mina-andajos <52796958+mina-andajos@users.noreply.github.com>
2022-08-08 15:37:21 +0200

    Add is_core_or_admin template filter

--- saskatoon/sitebase/templatetags/is_person.py
+++ saskatoon/sitebase/templatetags/is_person.py
@@ -1,4 +1,5 @@
 from django import template
+from member.models import AuthUser
 
 register = template.Library()
 
@@ -11,3 +12,8 @@ def is_person(actor):
         return True
     except:
         return False
+
+@register.filter(name="is_core_or_admin")
+def is_core_or_admin(user: AuthUser) -> bool:
+    """checks if the user making the request has the groups core or admin"""
+    return user.groups.filter(name__in=("admin", "core")).exists()
