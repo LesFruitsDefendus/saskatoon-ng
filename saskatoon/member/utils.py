6656a4323ee644b664615378ed259c37448f5c47
Qynn Schwaab <41802320+2ynn@users.noreply.github.com>
2024-04-25 19:46:33 -0400

    set up registration invite (#388)

---
+++ saskatoon/member/utils.py
@@ -0,0 +1,24 @@
+from django.core.mail import EmailMessage
+from logging import getLogger
+from member.models import AuthUser
+
+logger = getLogger('saskatoon')
+
+
+def send_reset_password_email(user: AuthUser, subject: str, message: str) -> bool:
+
+    password = AuthUser.objects.make_random_password(16)
+    user.set_password(password)
+    user.save()
+
+    mailto = user.email
+    email = EmailMessage(subject, message.format(password=password), None, [mailto])
+
+    try:
+        email.send()
+        logger.info("Successfully sent Reset Password email to %s", mailto)
+        return True
+    except Exception as e:
+        logger.error("Failed sending Reset Password email to %s. %s: %s",
+                     mailto, type(e), str(e))
+        return False
