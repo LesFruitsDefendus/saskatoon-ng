9604539cb678e9ae0261ef8e65b96650e4058a90
Qynn Schwaab <41802320+2ynn@users.noreply.github.com>
2024-04-25 18:55:25 -0400

    chore: cleanup views permissions (#389)
    * cleanup permissions
    
    * add admin role on superuser creation
    
    ---------
    
    Co-authored-by: ox <75142042+oxgr@users.noreply.github.com>

--- saskatoon/member/views.py
+++ saskatoon/member/views.py
@@ -2,7 +2,6 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.views.generic import CreateView, UpdateView
 from django.urls import reverse_lazy
-from django.shortcuts import render
 from django.utils.translation import gettext_lazy as _
 from .models import Person, Organization
 from harvest.models import Property
