09f634238fbfb8c5c108709dfd13679a177092e5
ox <75142042+oxgr@users.noreply.github.com>
2024-11-22 13:09:44 -0500

    remove redundant property field in equipment form

--- saskatoon/harvest/forms.py
+++ saskatoon/harvest/forms.py
@@ -610,22 +610,9 @@ class HarvestYieldForm(forms.ModelForm):
 
 
 class EquipmentForm(forms.ModelForm):
-    def clean(self):
-        cleaned_data = super(EquipmentForm, self).clean()
-        bool1 = bool(self.cleaned_data['property'])
-        bool2 = bool(self.cleaned_data['owner'])
-        if not (bool1 != bool2):
-            raise forms.ValidationError(
-                _('Fill in one of the two fields: property or owner.')
-            )
-        return cleaned_data
-
     class Meta:
         model = Equipment
         widgets = {
-            'property': autocomplete.ModelSelect2(
-                'property-autocomplete'
-            ),
             'owner': autocomplete.ModelSelect2(
                 'equipmentpoint-autocomplete',
             ),
