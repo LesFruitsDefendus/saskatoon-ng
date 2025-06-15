1995508472eecd6f3a544b5e1f82a7d109e7b529
Qynn Swaan <41802320+2ynn@users.noreply.github.com>
2025-06-15 16:15:19 -0400

    fix delayed warning in onboarding admin form

--- saskatoon/member/admin.py
+++ saskatoon/member/admin.py
@@ -432,13 +432,14 @@ class OnboardingAdmin(admin.ModelAdmin):
 
     def save_model(self, request, obj, form, change):
         """Make sure all_sent is False if users get added later on"""
+        super().save_model(request, obj, form, change)
         if obj.all_sent and obj.persons.filter(auth_user__password='').exists():
             obj.all_sent = False
+            obj.save()
             messages.warning(
                 request,
                 f"Some users in {obj} were not yet invited."
             )
-        super().save_model(request, obj, form, change)
 
     @admin.action(description="Send registration invite to selected group(s)")
     def send_invite(self, request, queryset):
