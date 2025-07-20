c8f4403fae5647428cb615447b1cd934e2b2fe06
Lucian Condrea <lucian@condrea.dev>
2025-07-20 19:14:38 -0400

    remove unnecessary env file copying

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 set -e
 
-# 1. Check if .env exists; if not, create it from template
-if [ ! -f /app/saskatoon/.env ]; then
-    echo "Creating .env from template"
-    cp /app/saskatoon/env.template /app/saskatoon/.env
-fi
-
-# 2. Check if requirements are installed (e.g., by checking for a known package)
+# Check if requirements are installed (e.g., by checking for a known package)
 if ! python -c "import django" &> /dev/null; then
     echo "Dependencies not found. Installing..."
     pip install --no-cache-dir '.[test]'
