3538ddf30736e7c9b335404a2d8e27a62cf1b34e
Lucian Condrea <lucian@condrea.dev>
2025-07-27 12:53:01 -0400

    wait for db to be ready before running fixtures init

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -19,8 +19,6 @@ fi
 echo "Ensuring dependencies are up to date..."
 pip install --no-cache-dir '.[test]'
 
-echo "Running database migrations and initializing fixtures..."
-saskatoon/fixtures/init
 
 # Wait for database to be ready and grant privileges needed for tests to work
 echo "Waiting for database to be ready..."
@@ -28,6 +26,9 @@ while ! mysql -h db -u root -proot -e "SELECT 1" &> /dev/null; do
     echo "Database not ready yet, waiting..."
     sleep 2
 done
+
+echo "Running database migrations and initializing fixtures..."
+saskatoon/fixtures/init
 
 echo "Granting database privileges..."
 mysql -h db -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'saskatoon'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
