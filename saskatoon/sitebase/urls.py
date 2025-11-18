2afb4342b2a4427bc9e268a3454a5c862286df72
Qynn Swaan <41802320+2ynn@users.noreply.github.com>
2025-11-17 23:09:59 -0500

    set up FAQ view

--- saskatoon/sitebase/urls.py
+++ saskatoon/sitebase/urls.py
@@ -1,42 +1,45 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
-from django.urls import path, re_path
+from django.urls import path
 from sitebase import views
 
 urlpatterns = [
-    url(r'^$', views.Index.as_view(), name='home'),
-    url(
-        'terms_conditions/',
+    url(r"^$", views.Index.as_view(), name="home"),
+    path("jsoncal", views.JsonCalendar.as_view(), name="calendarJSON"),
+    path("calendar/", views.Calendar.as_view(), name="calendar"),
+    path("faq/", views.FAQView.as_view(), name="faq"),
+    path("about/", views.Calendar.as_view(), name="about"),
+    path(
+        "terms_conditions/",
         views.TermsConditionsView.as_view(),
-        name='terms_conditions',
+        name="terms_conditions",
     ),
-    url(r'^jsoncal', views.JsonCalendar.as_view(), name='calendarJSON'),
-    re_path(r'^calendar/$', views.Calendar.as_view(), name='calendar'),
     path(
-        'equipment_points/',
+        "equipment_points/",
         views.EquipmentPointsPDFView.as_view(),
-        name='equipment-points',
+        name="equipment-points",
     ),
-    path('privacy_policy/', views.PrivacyPolicyView.as_view(), name='privacy_policy'),
+    path("privacy_policy/", views.PrivacyPolicyView.as_view(), name="privacy_policy"),
     path(
-        'volunteer_waiver/',
+        "volunteer_waiver/",
         views.VolunteerWaiverPDFView.as_view(),
-        name='volunteer-waiver',
+        name="volunteer-waiver",
     ),
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path("reset_password/", auth_views.PasswordResetView.as_view(), name="reset_password"),
     path(
-        'reset_password_sent/',
+        "reset_password_sent/",
         auth_views.PasswordResetDoneView.as_view(),
-        name='password_reset_done',
+        name="password_reset_done",
     ),
     path(
-        'reset/<uidb64>/<token>',
+        "reset/<uidb64>/<token>",
         auth_views.PasswordResetConfirmView.as_view(),
-        name='password_reset_confirm',
+        name="password_reset_confirm",
     ),
     path(
-        'reset_password_complete/',
+        "reset_password_complete/",
         auth_views.PasswordResetCompleteView.as_view(),
-        name='password_reset_complete',
+        name="password_reset_complete",
     ),
+    path("privacy_policy/", views.PrivacyPolicyView.as_view(), name="privacy_policy"),
 ]
