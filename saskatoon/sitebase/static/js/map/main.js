96e1c4dff88538e85272c6df59fc48b899913366
Patrick Marchand <mail@patrickmarchand.com>
2026-01-23 11:12:17 -0500

    First wip commit
    Contains a web component wrapping up leaflet

---
+++ saskatoon/sitebase/static/js/map/main.js
@@ -0,0 +1,82 @@
+class LeafletMap extends HTMLElement {
+
+    map = null
+    icon = null
+
+    initMap(shadow) {
+        const mapRoot = document.createElement('div');
+        mapRoot.id = 'map-root';
+        mapRoot.style = 'height: 84vh; width: 60vw;';
+        shadow.appendChild(mapRoot);
+        const layer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        });
+        const map = L.map(mapRoot, {
+            minZoom: 9,
+        }).addLayer(layer).setView([45.5088, -73.5617], 11);
+        const children = this.children;
+
+        this.icon = L.icon({
+            iconUrl: "/static/js/map/icon/marker-default.svg",
+            iconSize: [80, 80],
+            iconAnchor: [30, 20],
+            popupAnchor: [0, -10],
+        });
+
+        this.map = map;
+        for (let i = 0; i < children.length; i++) {
+            if (children[i].tagName === "LEAFLET-MARKER") {
+                this.processMarker(children[i]);
+            }
+        }
+
+    }
+
+    processMarker(node) {
+        const long = parseFloat(node.getAttributeNode('long').value);
+        const lat = parseFloat(node.getAttributeNode('lat').value);
+
+        if (long && lat && !isNaN(long) && !isNaN(lat)) {
+            L.marker([lat, long], { icon: this.icon }).addTo(this.map).bindPopup(node);
+        }
+    }
+
+    connectedCallback() {
+        const shadow = this.attachShadow({ mode: "open" });
+        const leafletcss = this.getLeafletStyleNode();
+        const leafletjs = this.getLeafletScriptNode();
+
+        leafletcss.onload = (ev) => {
+            shadow.appendChild(leafletjs);
+        }
+
+        leafletjs.onload = (ev) => {
+            this.initMap(shadow)
+        }
+
+        shadow.appendChild(leafletcss);
+    }
+
+    getLeafletStyleNode = () => {
+        let css = document.createElement('link');
+        css.href = 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css';
+        css.rel = 'stylesheet';
+        css.crossorigin = ""
+
+        return css;
+    }
+
+    getLeafletScriptNode = () => {
+        let js = document.createElement('script');
+        js.src = 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js';
+        js.integrity = 'sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=';
+        js.crossOrigin = '';
+        js.async = 'false';
+
+        return js;
+    }
+
+}
+
+customElements.define('leaflet-map', LeafletMap);
+customElements.define('leaflet-marker', class extends HTMLElement {})
