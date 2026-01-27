4ce48c022c6e1637fffb0c8f865b3f6146b0f501
Patrick Marchand <mail@patrickmarchand.com>
2026-01-27 09:14:40 -0500

    improvements to leaflet web component
     - better separation of concerns between map and marker
     - vendor leaflet
     - add icon / iconHover attributes for markers
     - tweaks to marker icon files

--- saskatoon/sitebase/static/js/map/main.js
+++ saskatoon/sitebase/static/js/map/main.js
@@ -1,64 +1,34 @@
-class LeafletMap extends HTMLElement {
+import "./leaflet.js";
 
+class LeafletMap extends HTMLElement {
     map = null
-    icon = null
 
     initMap(shadow) {
         const mapRoot = document.createElement('div');
         mapRoot.id = 'map-root';
         const style = this.getAttributeNode('style');
         if (style) {
-            console.log(style)
             mapRoot.style = style.value;
         }
         shadow.appendChild(mapRoot);
         const layer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         });
-        const map = L.map(mapRoot, {
+        this.map = L.map(mapRoot, {
             minZoom: 9,
         }).addLayer(layer).setView([45.5088, -73.5617], 11);
 
-        const children = this.children;
-
-        this.icon = L.icon({
-            iconUrl: "/static/js/map/icon/marker-default.svg",
-            iconSize: [30, 30],
-            iconAnchor: [20, 10],
-            popupAnchor: [-5, -10],
+        this.addEventListener('markerCreated', (event) => {
+            event.detail.addTo(this.map);
         });
-
-        this.map = map;
-        for (let i = 0; i < children.length; i++) {
-            if (children[i].tagName === "LEAFLET-MARKER") {
-                this.processMarker(children[i]);
-            }
-        }
-    }
-
-    processMarker(node) {
-        const long = parseFloat(node.getAttributeNode('long').value);
-        const lat = parseFloat(node.getAttributeNode('lat').value);
-
-        if (long && lat && !isNaN(long) && !isNaN(lat)) {
-            L.marker([lat, long], { icon: this.icon }).addTo(this.map).bindPopup(node);
-        }
     }
 
     connectedCallback() {
         const shadow = this.attachShadow({ mode: "open" });
         const leafletcss = this.getLeafletStyleNode();
-        const leafletjs = this.getLeafletScriptNode();
-
-        leafletcss.onload = (ev) => {
-            shadow.appendChild(leafletjs);
-        }
-
-        leafletjs.onload = (ev) => {
-            this.initMap(shadow)
-        }
 
         shadow.appendChild(leafletcss);
+        this.initMap(shadow)
     }
 
     getLeafletStyleNode = () => {
@@ -82,5 +52,66 @@ class LeafletMap extends HTMLElement {
 
 }
 
+class LeafletMarker extends HTMLElement {
+    icon = null
+    iconHover = null
+    marker = null
+
+    constructor() {
+        super();
+
+        const long = parseFloat(this.getAttributeNode('long').value);
+        const lat = parseFloat(this.getAttributeNode('lat').value);
+        const icon = this.getAttributeNode('icon');
+        const iconHover = this.getAttributeNode('icon-hover');
+
+        if (!long || !lat || isNaN(long) || isNaN(lat)) {
+            throw new Error("Marker needs lat and long")
+        }
+
+        this.icon = L.icon({
+            iconUrl: icon ? icon.value : '/static/js/map/icon/marker-default.svg',
+            iconSize: [30, 30],
+            iconAnchor: [20, 10],
+            popupAnchor: [-5, -10],
+        });
+
+        this.iconHover = L.icon({
+            iconUrl: iconHover ? iconHover.value : '/static/js/map/icon/marker-hover.svg',
+            iconSize: [36, 36],
+            iconAnchor: [23, 13],
+            popupAnchor: [-5, -10],
+        });
+
+        this.marker = L.marker([lat, long], { icon: this.icon });
+    }
+
+    connectedCallback() {
+            const popup = document.createElement('div');
+            popup.append(...this.children);
+            this.marker.bindPopup(popup);
+
+            this.marker.addEventListener('mouseover', (event) => {
+                this.marker.setIcon(this.iconHover);
+            });
+
+            this.marker.addEventListener('mouseout', (event) => {
+                if (!this.marker.isPopupOpen()) {
+                    this.marker.setIcon(this.icon);
+                }
+            });
+
+            this.marker.addEventListener('popupclose', (event) => {
+                this.marker.setIcon(this.icon);
+            });
+            const markerCreated = new CustomEvent('markerCreated', {
+                bubbles: true,
+                detail: this.marker,
+            });
+
+            this.dispatchEvent(markerCreated);
+    }
+}
+
 customElements.define('leaflet-map', LeafletMap);
-customElements.define('leaflet-marker', class extends HTMLElement {})
+customElements.define('leaflet-marker', LeafletMarker)
