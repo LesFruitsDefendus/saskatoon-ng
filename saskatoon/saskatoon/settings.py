5fe31fc186244d31fe28953212f4ca9aaa28583c
Patrick Marchand <mail@patrickmarchand.com>
2026-02-09 03:40:33 -0500

    Add admin functionality for org maps

--- saskatoon/saskatoon/settings.py
+++ saskatoon/saskatoon/settings.py
@@ -74,6 +74,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'leaflet',
+    'djgeojson',
     'sitebase',
     'member',
     'harvest',
