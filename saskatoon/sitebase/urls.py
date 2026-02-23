87f38b14f291ece192c8b07e874c7a030974f69f
Qynn Schwaab <qynn@commonsense.dev>
2026-02-23 15:26:07 +0100

    upgrade to Django 4.0 and cleanup dependencies

--- saskatoon/sitebase/urls.py
+++ saskatoon/sitebase/urls.py
@@ -1,16 +1,15 @@
-from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from django.urls import path, re_path
 from sitebase import views
 
 urlpatterns = [
-    url(r'^$', views.Index.as_view(), name='home'),
-    url(
+    re_path(r'^$', views.Index.as_view(), name='home'),
+    re_path(
         'terms_conditions/',
         views.TermsConditionsView.as_view(),
         name='terms_conditions',
     ),
-    url(r'^jsoncal', views.JsonCalendar.as_view(), name='calendarJSON'),
+    re_path(r'^jsoncal', views.JsonCalendar.as_view(), name='calendarJSON'),
     re_path(r'^calendar/$', views.Calendar.as_view(), name='calendar'),
     path(
         'equipment_points/',
