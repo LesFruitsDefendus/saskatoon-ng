419be2bef4129c6abd43935f44c0b66af0320098
Qynn Schwaab <41802320+2ynn@users.noreply.github.com>
2026-05-07 22:35:20 -0400

    chore: cleanup pdf paths (#682)
    * add volunteer_waiver pdf
    
    * delete equipment_points/ pdf view

--- saskatoon/saskatoon/settings.py
+++ saskatoon/saskatoon/settings.py
@@ -208,8 +208,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Statically served PDF files
-EQUIPMENT_POINTS_PDF_PATH = os.path.join(MEDIA_ROOT, "guides/equipment_points.pdf")
-VOLUNTEER_WAIVER_PDF_PATH = os.path.join(MEDIA_ROOT, "guides/volunteer_waiver.pdf")
+VOLUNTEER_WAIVER_PDF_PATH = os.path.join(MEDIA_ROOT, "volunteer_waiver.pdf")
 
 # EMAIL SERVER
 EMAIL_BACKEND = os.getenv('SASKATOON_EMAIL_BACKEND')
