2a7552bcddcd8bd341b91b870bc7f9b6b2475608
Qynn Schwaab <41802320+2ynn@users.noreply.github.com>
2026-07-09 01:55:24 -0400

    amend: add is not duplicate email filter (#748)

--- saskatoon/sitebase/admin_filters.py
+++ saskatoon/sitebase/admin_filters.py
@@ -9,18 +9,21 @@ class EmailIsDuplicateAdminFilter(SimpleListFilter):
     """Checks if Email object is identical to previous one"""
 
     title = 'Duplicate Filter'
-    parameter_name = 'login'
+    parameter_name = 'dup'
     default_value: Optional[Email] = None
 
     def lookups(self, request, model_admin):
         return [
-            ('dup', _('Duplicates')),
+            ('yes', _('Duplicates')),
+            ('no', _('Originals')),
         ]
 
     def queryset(self, request, queryset):
-        if self.value() == 'dup':
+        if self.value():
             duplicates = [e.id for e in queryset if e.is_duplicate]
-            print("DUPLICATES!", duplicates)
-            return queryset.filter(id__in=duplicates)
+            if self.value() == 'yes':
+                return queryset.filter(id__in=duplicates)
+            if self.value() == 'no':
+                return queryset.exclude(id__in=duplicates)
 
         return queryset
