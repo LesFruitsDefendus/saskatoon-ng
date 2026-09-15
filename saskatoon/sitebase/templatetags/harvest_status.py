b86c62c004e845196321a392e8dae2b152248f35
GlaabJ <jonglaab@gmail.com>
2026-09-15 16:03:05 -0400

    fix: added tooltips to harvest distribution button/badge

--- saskatoon/sitebase/templatetags/harvest_status.py
+++ saskatoon/sitebase/templatetags/harvest_status.py
@@ -255,26 +255,33 @@ def harvest_recipient_attributes(status: Optional[str], direction: str = "top") 
         return default
 
     help_text = {
-        status: text
-        for statuses, text in [
+        t[0]: t[1]
+        for t in [
             (
-                [
-                    Harvest.Status.ADOPTED.value,
-                    Harvest.Status.SCHEDULED.value,
-                    Harvest.Status.CANCELLED.value,
-                    Harvest.Status.ORPHAN.value,
-                ],
-                _("A harvest must be marked as Ready before you can complete fruit distribution"),
+                Harvest.Status.ORPHAN.value,
+                _("Harvest needs a pick leader to adopt it"),
             ),
             (
-                [
-                    Harvest.Status.READY.value,
-                    Harvest.Status.SUCCEEDED.value,
-                ],
+                Harvest.Status.ADOPTED.value,
+                _("A Harvest must be ready before adding recipients"),
+            ),
+            (
+                Harvest.Status.SCHEDULED.value,
+                _("A Harvest must be ready before adding recipients"),
+            ),
+            (
+                Harvest.Status.READY.value,
                 _("Add a new recipient to record fruit distribution for this harvest"),
             ),
+            (
+                Harvest.Status.SUCCEEDED.value,
+                _("Add a new recipient to record fruit distribution for this harvest"),
+            ),
+            (
+                Harvest.Status.CANCELLED.value,
+                _("Harvest is cancelled, it will need to be rescheduled by the pick leader"),
+            ),
         ]
-        for status in statuses
     }.get(status, default)
 
     return (
