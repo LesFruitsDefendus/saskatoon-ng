f212fabaa96d46286bc2d9f7a77c3a1a16dc11d3
Qynn Swaan <41802320+2ynn@users.noreply.github.com>
2026-09-23 22:11:46 -0400

    reset test_Autocomplete_get_queryset_none

--- saskatoon/unittests/member/test_autocomplete.py
+++ saskatoon/unittests/member/test_autocomplete.py
@@ -32,6 +32,16 @@ def test_Autocomplete_init(Autocomplete):
     assert isinstance(autocomplete, Autocomplete)
 
 
+@pytest.mark.parametrize("Autocomplete", autocomplete_classes)
+def test_Autocomplete_get_queryset_none(Autocomplete):
+    """Test that get_queryset method can be called and returns no results"""
+
+    autocomplete = Autocomplete()
+    results = autocomplete.get_queryset()
+
+    assert results.count() == 0
+
+
 @pytest.mark.django_db
 def test_neighborhood_autocomplete_search(client):
     neighborhoods = ["Pointe-Saint-Charles", "Saint-Henri", "Verdun"]
